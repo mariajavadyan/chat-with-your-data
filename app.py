4fcae3d760ffebd877e32ad16829c09817c2452a
Maria Javadyan <maria.javadyan02@gmail.com>
2025-05-18 15:07:32 +0400

    final final version

--- app.py
+++ app.py
@@ -144,13 +144,6 @@ def handle_userinput(user_question):
 
     st.session_state.chat_history = response['chat_history']
 
-    # for i, message in enumerate(st.session_state.chat_history):
-    #     if i % 2 == 0:
-    #         st.write(user_template.replace(
-    #             "{{MSG}}", message.content), unsafe_allow_html=True)
-    #     else:
-    #         st.write(bot_template.replace(
-    #             "{{MSG}}", message.content), unsafe_allow_html=True)
 
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
